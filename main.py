31e9ead742240b51d8d2c871e894ea136b962da3
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2023-05-22 18:10:49 -0300

    modificação para adicionar mais dados aos requests

--- main.py
+++ main.py
@@ -1,24 +1,45 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
 
 app = Flask(__name__)
 api = Api(app)
 
 video_put_args = reqparse.RequestParser()
-video_put_args.add_argument("nome", type=str, help="Nome do vídeo é obrigatório.", required=True)
+video_put_args.add_argument(
+    "nome", type=str, help="Nome do vídeo é obrigatório.", required=True
+)
 video_put_args.add_argument("views", type=str, help="Views do vídeo")
 video_put_args.add_argument("likes", type=str, help="Likes do vídeo")
 
 videos = {}
 
+
+def abort_not_video_id(video_id):
+    if video_id not in videos:
+        abort(404, message="Vídeo não encontrado")
+
+
+def abort_video_id(video_id):
+    if video_id in videos:
+        abort(409, message="Vídeo já existente")
+
+
 class Video(Resource):
     def get(self, video_id):
+        abort_not_video_id(video_id)
         return videos[video_id]
-    
+
     def put(self, video_id):
+        abort_video_id(video_id)
         args = video_put_args.parse_args()
         videos[video_id] = args
         return videos[video_id], 201
+
+    def delete(self, video_id):
+        abort_not_video_id(video_id)
+        del videos[video_id]
+        return "", 204
+
 
 api.add_resource(Video, "/video/<int:video_id>")
 
--- test.py
+++ test.py
@@ -2,8 +2,19 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "video/1", {"likes": 10, "nome": "Andrey", "views": 7858392})
-print(response.json())
+data = [
+    {"likes": 53613, "nome": "Gustavo", "views": 7858392},
+    {"likes": 5471, "nome": "Receita de Bolo", "views": 51254},
+    {"likes": 67234, "nome": "Guia de Windows", "views": 7858392},
+]
+
+for i in range(len(data)):
+    response = requests.put(BASE + "video/1" + str(i), data[i])
+    print(response.json())
+
 input()
-response = requests.get(BASE + "video/1")
+response = requests.delete(BASE + "video/0")
+print(response)
+input()
+response = requests.get(BASE + "video/2")
 print(response.json())
