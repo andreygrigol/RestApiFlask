a8332f1b0827d28bb4cbb08f5f346bc6dff53a7f
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2023-05-23 09:32:04 -0300

    API construída, falta apenas o front

--- test.py
+++ test.py
@@ -3,18 +3,10 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 data = [
-    {"likes": 53613, "nome": "Gustavo", "views": 7858392},
-    {"likes": 5471, "nome": "Receita de Bolo", "views": 51254},
-    {"likes": 67234, "nome": "Guia de Windows", "views": 7858392},
+    {"likes": 53613, "titulo": "Gustavo", "views": 7858392},
+    {"likes": 5471, "titulo": "Receita de Bolo", "views": 51254},
+    {"likes": 67234, "titulo": "Guia de Windows", "views": 7858392},
 ]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
-
-input()
-response = requests.delete(BASE + "video/0")
-print(response)
-input()
-response = requests.get(BASE + "video/2")
+response = requests.patch(BASE + "video/2", {"views":999})
 print(response.json())
