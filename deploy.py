6343848de38437062f4fc15dc18b9e612b5aef49
AndreyRibeiro <andrey.grigoletto2@gmail.com>
2023-05-24 13:05:07 -0300

    Atualização de arquivo

--- deploy.py
+++ deploy.py
@@ -3,7 +3,7 @@ import requests
 BASE = "http://127.0.0.1:5000/"
 
 data = [
-    {"likes": 53613, "titulo": "Gustavo", "views": 7858392},
+    {"likes": 53613, "titulo": "Gatos fofos", "views": 7858392},
     {"likes": 5471, "titulo": "Receita de Bolo", "views": 51254},
     {"likes": 67234, "titulo": "Guia de Windows", "views": 7858392},
 ]
